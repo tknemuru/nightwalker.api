1fd7a8d5065ce8f63adeb156b1baa40f7f035628
tknemuru <kojikoji.max.5252@gmail.com>
2014-12-14 00:15:02 +0900

    initial commit

---
+++ src/main/java/nightwalker/api/Application.java
@@ -0,0 +1,18 @@
+package nightwalker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by takashi on 2014/12/13.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/nightwalker/api/models/Widget.java
@@ -0,0 +1,30 @@
+package nightwalker.api.models;
+
+/**
+ * Created by takashi on 2014/12/13.
+ */
+public class Widget {
+    private String type;
+
+    private int length;
+
+    private int height;
+
+    public Widget(String type, int length, int height) {
+        this.type = type;
+        this.length = length;
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
